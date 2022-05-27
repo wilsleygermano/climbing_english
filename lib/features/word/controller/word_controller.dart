9410ddae8348d1f4f779a00c37617304157afa54
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-05-27 09:10:34 -0300

    WIP API word page controller

--- lib/features/word/controller/word_controller.dart
+++ lib/features/word/controller/word_controller.dart
@@ -1,4 +1,5 @@
 import 'package:climbing_english/core/model/word_model.dart';
+import 'package:climbing_english/features/word/model/searched_word_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:mobx/mobx.dart';
@@ -15,31 +16,34 @@ abstract class _WordControllerBase with Store {
   String word = "Carregando...";
 
   @observable
-  String firstMeaning = "Carregando...";
-
-  @observable
   String phonetic = "Carregando...";
 
   @observable
   String pronounceURL = "";
 
+  @observable
+  List<Definition> definitions = [];
+
   @action
   Future<void> getWordTyped(String wordTyped) async {
     WordModel wordModel;
+    List<Definition> apiDefinitions;
     try {
       final dio = Dio();
       final apiURL = DictionaryApiUrl.url + wordTyped;
-      var response = await dio.get(apiURL);
+      var response = await dio.get<List<Map<String, dynamic>>>(apiURL);
+      // Retorna uma lista de mapas. 
       final json = response.data;
       // final decodedJson = jsonDecode(json) as Map<String, dynamic>;
-      wordModel = WordModel.fromJson(json);
+      wordModel = WordModel.fromJson(json!);
+      apiDefinitions = json[0]["meanings"][0]["definitions"].map((map) => Definition.fromJson(map)).toList();
+
       word = wordModel.word!;
-      firstMeaning = wordModel.wordOfTheDayDefinition!;
       phonetic = wordModel.phonetic!;
       pronounceURL = wordModel.phoneticUrl!;
+      definitions = apiDefinitions;
     } on DioError catch (e) {
       word = "Houve um erro: ${e.error}";
-      firstMeaning = "Houve um erro: ${e.error}";
       phonetic = "Houve um erro: ${e.error}";
       pronounceURL = "Houve um erro: ${e.error}";
     }
