df9add12a51b6b214204a7be30bea1353ed93345
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-05-30 15:52:24 -0300

    Finishe iOS setup

--- lib/core/widgets/app_fonts.dart
+++ lib/core/widgets/app_fonts.dart
@@ -6,7 +6,7 @@ class AppFonts {
   static TextStyle appfont20 = GoogleFonts.lato(fontSize: 20);
   static TextStyle appfont24 = GoogleFonts.lato(fontSize: 24);
   static TextStyle textfont18 = GoogleFonts.cormorant(fontSize: 18);
-  static TextStyle textfont22 = GoogleFonts.cormorant(fontSize: 22);
+  static TextStyle textfont22 = GoogleFonts.cormorant(fontSize: 20);
   static TextStyle textfont26 = GoogleFonts.cormorant(fontSize: 26);
   static TextStyle textfont32 = GoogleFonts.cormorant(fontSize: 32);
   static TextStyle textinfo = GoogleFonts.righteous(fontSize: 32);
